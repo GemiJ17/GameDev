ad036e57dbda0a4b2d37bca281f692e64fb0a9d0
GemiJ17 <nutchar.sp@gmail.com>
2025-05-23 13:08:44 +0700

    zzz

---
+++ Assets/Scripts/GameplayUIUpdater.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameplayUIUpdater : MonoBehaviour
+{
+    public Text hpText;
+    public Text timerText;
+    public Text scoreText;
+    public int a = 0;
+
+    private MainLogic mainLogic;
+
+    void Start()
+    {
+        mainLogic = FindObjectOfType<MainLogic>();
+    }
+
+    void Update()
+    {
+        if (mainLogic == null) return;
+
+        int hp = Mathf.Max(mainLogic.GetHP(), 0);
+        hpText.text = $"HP: {hp}";
+
+        float time = Mathf.Max(mainLogic.GetTimeRemaining(), 0);
+        timerText.text = $"Time: {time:F1}s";
+
+        scoreText.text = $"Score: {mainLogic.GetScore()}";
+    }
+}
